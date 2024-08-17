872916e7dfff68febe8509282e6880e87805c822
Gabriel Soncin <gabrielsoncindev@gmail.com>
2024-08-17 20:21:29 +0000

    updating commit

---
+++ src/pages/TrainingJourney/TrainingJourney.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './TrainingJourney.css';
+
+const TrainingJourney: React.FC = () => {
+  return (
+    <div className="training-journey">
+      <div className="header">
+        <div className="menu">
+          <span className="menu-icon">≡</span>
+          <span className="menu-text">menu</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TrainingJourney;
