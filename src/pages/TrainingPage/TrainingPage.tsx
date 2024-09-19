99c443c3b207d7f6b6355a9e51f3edd486cdfc6a
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-18 22:01:42 -0300

    API

--- src/pages/TrainingPage/TrainingPage.tsx
+++ src/pages/TrainingPage/TrainingPage.tsx
@@ -1,3 +1,74 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import TrainingList from "../../components/TrainingList/TrainingList";
+import "./TrainingPage.css";
+import profileImage from "../../assets/user.png";
+import Background from "../../components/Background/Background";
+import axios from "axios";
+
+interface ApiResponse {
+  mensagem: string;
+}
+
+const TrainingPage: React.FC = () => {
+  const navigate = useNavigate();
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [data, setData] = useState<ApiResponse | null>(null);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get<ApiResponse>(
+          "http://localhost:5000/api/exemplo"
+        );
+        setData(response.data);
+      } catch (error) {
+        console.error("Erro ao buscar dados:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="training-page">
+      <Background />
+      <div className="header">
+        <div className="menu" onClick={toggleMenu}>
+          <span className="menu-icon">≡</span>
+          <span className="menu-text">menu</span>
+          {menuOpen && (
+            <div className="dropdown-menu">
+              <button onClick={() => navigate("/")}>Sair</button>
+              <button onClick={() => navigate("/journey")}>Jornada</button>
+              <button onClick={() => navigate("/iapage")}>GALLI</button>
+            </div>
+          )}
+        </div>
+      </div>
+      <div className="profile-section">
+        <h1 className="username">
+          NOME
+          <br />
+          USUÁRIO
+        </h1>
+        <img src={profileImage} alt="User Profile" className="profile-image" />
+      </div>
+      <div className="training-list-container">
+        <TrainingList />
+        {data ? <p>{data.mensagem}</p> : <p>Carregando dados do back-end...</p>}
+      </div>
+    </div>
+  );
+};
+export default TrainingPage;
+
+/*export default TrainingPage;
+
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import TrainingList from "../../components/TrainingList/TrainingList";
@@ -41,6 +112,4 @@ const TrainingPage: React.FC = () => {
       </div>
     </div>
   );
-};
-
-export default TrainingPage;
+};*/
